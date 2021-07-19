b73c4a510d06a308dbec8502aa9e1046da45dcba
Kacper Walter <kacperwalter@MacBook-Pro-Kacper-W.local>
2021-07-19 23:02:44 +0200

    JS for hover ready

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     
+    const serviceCards = document.querySelectorAll('.service-card');
+
+    serviceCards.forEach((card) => {
+        card.addEventListener('mouseover', (event) => {
+            card.childNodes[3].classList.add('service-card__copy-box--expanded');
+        })
+
+        card.addEventListener('mouseleave', (event) => {
+            card.childNodes[3].classList.remove('service-card__copy-box--expanded');
+        })
+    })
+
 });
